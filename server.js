4d3428b546fb60138f5c5cfbb806e64eab33e6df
Sergyoga2 <a1040798s@gmail.com>
2025-12-25 12:18:05 +0300

    Add local preview server

---
+++ server.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const port = process.env.PORT || 8000;
+const publicDir = __dirname;
+
+const mimeTypes = {
+  ".html": "text/html",
+  ".css": "text/css",
+  ".js": "text/javascript",
+  ".json": "application/json",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
+  ".svg": "image/svg+xml",
+  ".ico": "image/x-icon"
+};
+
+const server = http.createServer((req, res) => {
+  const requestUrl = req.url === "/" ? "/index.html" : req.url;
+  const filePath = path.join(publicDir, decodeURIComponent(requestUrl));
+
+  if (!filePath.startsWith(publicDir)) {
+    res.writeHead(403);
+    res.end("Forbidden");
+    return;
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("Not Found");
+      return;
+    }
+
+    const ext = path.extname(filePath).toLowerCase();
+    const contentType = mimeTypes[ext] || "application/octet-stream";
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(data);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Preview server running at http://localhost:${port}`);
+});
